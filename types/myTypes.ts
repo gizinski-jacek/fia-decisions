4492ff67155702d4714775e2bcb14aeda3be24c9
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-09 01:10:53 +0200

    Adding DecisionModel type

---
+++ types/myTypes.ts
@@ -0,0 +1,24 @@
+export interface DecisionModel {
+	decision_type: string;
+	grand_prix: string;
+	weekend: string;
+	heading: {
+		From: string;
+		To: string;
+		Document: string;
+		Date: string;
+		Time: string;
+	};
+	content: {
+		Headline: string;
+		Driver: string;
+		Competitor: string;
+		Time: string;
+		Session: string;
+		Fact: string;
+		Iffence: string;
+		Decision: string;
+		Reason: string;
+	};
+	stewards: string[];
+}
