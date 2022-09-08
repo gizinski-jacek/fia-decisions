a3e6d0feb96852256b2fa02744cf81dfc10dddbe
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-09 01:08:26 +0200

    Moving Head, Footer and main to Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Footer from '../components/Footer';
 
 const Home: NextPage = () => {
-	return (
-		<div>
-			<Head>
-				<title>FIA Decisions</title>
-				<meta name='description' content='FIA Decisions' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<main>Hello</main>
-			<Footer />
-		</div>
-	);
+	return <div>Welcome to home page</div>;
 };
 
 export default Home;
