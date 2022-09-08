613889de99f57aad5663657a3f71ae4b7de64719
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-09 01:06:57 +0200

    Adding Layout component
    Wrapping App with ThemeContextProvider.
    Rendering Drawer, Footer and children.

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import { ThemeContextProvider } from '../hooks/ThemeProvider';
+import Drawer from './Drawer';
+import Footer from './Footer';
+
+interface Props {
+	children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+	return (
+		<ThemeContextProvider>
+			<Head>
+				<title>FIA Decisions</title>
+				<meta name='description' content='FIA Decisions' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Drawer />
+			<main>{children}</main>
+			<Footer />
+		</ThemeContextProvider>
+	);
+};
+
+export default Layout;
