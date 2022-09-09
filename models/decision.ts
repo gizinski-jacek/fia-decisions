87ca8b90aa7eaf8ba1ecb9844215be4a48ce704d
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-09 19:53:34 +0200

    Updating Decision model.

--- models/decision.ts
+++ models/decision.ts
@@ -5,106 +5,28 @@ const Schema = mongoose.Schema;
 
 const DecisionSchema = new Schema<DecisionModel>(
 	{
-		doc_type: {
-			type: String,
-			trim: true,
-			required: true,
-		},
-		doc_name: {
-			type: String,
-			trim: true,
-			required: true,
-		},
-		grand_prix: {
-			type: String,
-			trim: true,
-			required: true,
-		},
-		penalty: {
-			type: String,
-			trim: true,
-			required: true,
-		},
-		weekend: {
-			type: String,
-			trim: true,
-			required: true,
-		},
+		doc_type: { type: String, trim: true, required: true },
+		doc_name: { type: String, trim: true, required: true },
+		grand_prix: { type: String, trim: true, required: true },
+		penalty: { type: String, trim: true, required: true },
+		weekend: { type: String, trim: true, required: true },
 		heading: {
-			From: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			To: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Document: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Date: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Time: {
-				type: String,
-				trim: true,
-				required: true,
-			},
+			From: { type: String, trim: true, required: true },
+			To: { type: String, trim: true, required: true },
+			Document: { type: String, trim: true, required: true },
+			Date: { type: String, trim: true, required: true },
+			Time: { type: String, trim: true, required: true },
 		},
 		content: {
-			Headline: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Driver: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Competitor: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Time: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Session: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Fact: [
-				{
-					type: String,
-					trim: true,
-					required: true,
-				},
-			],
-			Offence: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Decision: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			Reason: {
-				type: String,
-				trim: true,
-				required: true,
-			},
+			Headline: { type: String, trim: true, required: true },
+			Driver: { type: String, trim: true, required: true },
+			Competitor: { type: String, trim: true, required: true },
+			Time: { type: String, trim: true, required: true },
+			Session: { type: String, trim: true, required: true },
+			Fact: [{ type: String, trim: true, required: true }],
+			Offence: { type: String, trim: true, required: true },
+			Decision: [{ type: String, trim: true, required: true }],
+			Reason: { type: String, trim: true, required: true },
 		},
 		stewards: [{ type: String, trim: true, required: true }],
 	},
