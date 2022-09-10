dbf1956e584f94b595a5f518aa7080371a87b727
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-11 01:46:35 +0200

    Moving sorting job to db query in gSSR.
    Removing fetch button.

--- pages/f1/index.tsx
+++ pages/f1/index.tsx
@@ -1,8 +1,6 @@
 import type { GetServerSidePropsContext, NextPage } from 'next';
-import axios from 'axios';
 import connectMongo from '../../lib/mongo';
 import Decision from '../../models/decision';
-import { useState } from 'react';
 import { DecisionMongoDBModel } from '../../types/myTypes';
 import F1DocWrapper from '../../components/wrappers/F1DocWrapper';
 
@@ -11,30 +9,12 @@ interface Props {
 }
 
 const F1: NextPage<Props> = ({ data }) => {
-	const testFetch = async () => {
-		try {
-			const res = await axios.get(
-				'/api/f1/force-update-all/decisions-offences'
-			);
-			console.log(res.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
 	return (
 		<div>
-			<button onClick={testFetch}>Test Fetch - Dont Abuse</button>
 			<div className='m-2'>
-				{data
-					.sort((a, b) => {
-						const dateB = new Date(`${b.heading.Date} ${b.heading.Time}`);
-						const dateA = new Date(`${a.heading.Date} ${a.heading.Time}`);
-						return dateB - dateA;
-					})
-					.map((d) => (
-						<F1DocWrapper key={d._id} data={d} />
-					))}
+				{data.map((d) => (
+					<F1DocWrapper key={d._id} data={d} />
+				))}
 			</div>
 		</div>
 	);
@@ -45,7 +25,9 @@ export const getServerSideProps = async (
 ) => {
 	try {
 		await connectMongo();
-		const decisionList: DecisionMongoDBModel[] = await Decision.find().exec();
+		const decisionList: DecisionMongoDBModel[] = await Decision.find()
+			.sort({ doc_date: -1 })
+			.exec();
 		return {
 			props: { data: JSON.parse(JSON.stringify(decisionList)) },
 		};
