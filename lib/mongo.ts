684ebe77e38021c259d2d6268841969c71a8aaad
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-11 01:44:52 +0200

    Updating mongo connect function

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,14 +1,40 @@
 import mongoose from 'mongoose';
 
-const options = {};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!process.env.MONGODB_URI) {
+if (!MONGODB_URI) {
 	throw new Error(
 		'Please define the MONGODB_URI environment variable inside .env.local'
 	);
 }
 
-const connectMongo = async () =>
-	mongoose.connect(process.env.MONGODB_URI as string, options);
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectMongo = async () => {
+	if (cached.conn) {
+		return cached.conn;
+	}
+
+	if (!cached.promise) {
+		const opts = {
+			bufferCommands: false,
+		};
+
+		cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+			return mongoose;
+		});
+	}
+	cached.conn = await cached.promise;
+	return cached.conn;
+};
 
 export default connectMongo;
