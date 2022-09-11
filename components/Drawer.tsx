55b8d7933a9f2655fff3c80f1ba7e07b944872e3
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-11 15:52:04 +0200

    Hiding Theme toggle button for now.
    Adding update button and request handler

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import Link from 'next/link';
 import { useContext } from 'react';
+import { Button } from 'react-bootstrap';
 import { ThemeContext } from '../hooks/ThemeProvider';
 
 const Drawer = () => {
@@ -9,25 +11,45 @@ const Drawer = () => {
 		toggleTheme();
 	};
 
+	const handleRequestUpdate = async () => {
+		try {
+			const res = await axios.get('/api/f1/force-update/decisions-offences');
+			console.log(res.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<nav>
-			<ul>
-				<li>
-					<button type='button' onClick={handleToggleTheme}>
+			<ul className='nav nav-pills nav-fill'>
+				{/* <li className='nav-item'>
+					<button
+						type='button'
+						className='nav-link active'
+						onClick={handleToggleTheme}
+					>
 						Toggle Theme
 					</button>
+				</li> */}
+				<li className='nav-item'>
+					<Button
+						variant='outline-dark'
+						size='sm'
+						onClick={handleRequestUpdate}
+					>
+						Request Update
+					</Button>
 				</li>
-				<li>
-					<Link href={'/'}>Home</Link>
+				<li className='nav-item'>
+					<Link href={'/'} className='nav-link'>
+						Home
+					</Link>
 				</li>
-				<li>
-					<Link href={'/f1'}>F1</Link>
-				</li>
-				<li>
-					<Link href={'/f2'}>F2</Link>
-				</li>
-				<li>
-					<Link href={'/f3'}>F3</Link>
+				<li className='nav-item'>
+					<Link href={'/f1'} className='nav-link'>
+						F1
+					</Link>
 				</li>
 			</ul>
 		</nav>
