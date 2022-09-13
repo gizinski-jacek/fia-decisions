b7f20a9254ca8e636cd299e9da2f04be7b7d3ae1
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-13 22:10:52 +0200

    Updating types.

--- types/myTypes.ts
+++ types/myTypes.ts
@@ -1,4 +1,9 @@
 export interface TransformedPDFData {
+	doc_type: string;
+	doc_name: string;
+	doc_date: Date;
+	penalty_type: string;
+	grand_prix: string;
 	weekend: string;
 	document_info: {
 		From: string;
@@ -21,14 +26,26 @@ export interface TransformedPDFData {
 	stewards: string[];
 }
 
-export interface DecisionModel extends TransformedPDFData {
-	doc_type: string;
-	doc_name: string;
-	doc_date: Date;
-	grand_prix: string;
-	penalty_type: string;
+export interface DocumentInfo {
+	From: string;
+	To: string;
+	Document: string;
+	Date: string;
+	Time: string;
 }
 
-export interface DecisionMongoDBModel extends DecisionModel {
+export interface IncidentInfo {
+	Headline: string;
+	Driver: string;
+	Competitor: string;
+	Time: string;
+	Session: string;
+	Fact: string;
+	Offence: string[];
+	Decision: string[];
+	Reason: string;
+}
+
+export interface DecisionModel extends TransformedPDFData {
 	_id: string;
 }
