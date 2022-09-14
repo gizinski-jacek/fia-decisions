37c9209d79ae6de4850f325efad6a4ffb2832ae1
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-14 15:35:24 +0200

    Updating links and adding fiaDomain link

--- lib/myData.ts
+++ lib/myData.ts
@@ -1,5 +1,7 @@
 import { DatabaseNameList, FiaPageList } from '../types/myTypes';
 
+export const fiaDomain: string = 'https://www.fia.com';
+
 export const dbNameList: DatabaseNameList = {
 	f1_2022_db: '2022_F1_WC_Docs',
 	f2_2022_db: '2022_F2_WC_Docs',
@@ -10,7 +12,7 @@ export const fiaPageList: FiaPageList = {
 	f1_2022_page:
 		'https://www.fia.com/documents/championships/fia-formula-one-world-championship-14/season/season-2022-2005',
 	f2_2022_page:
-		'https://www.fia.com/documents/season/season-2022-2005/championships/formula-2-championship-44',
+		'https://www.fia.com/documents/championships/formula-2-championship-44/season/season-2022-2005',
 	f3_2022_page:
-		'https://www.fia.com/documents/season/season-2022-2005/championships/fia-formula-3-championship-1012',
+		'https://www.fia.com/documents/championships/fia-formula-3-championship-1012/season/season-2022-2005',
 };
