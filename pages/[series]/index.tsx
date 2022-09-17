b2140b87343888c0fe962e67205625b680f7bdc7
Jacek Giziński <jacektrg+github@gmail.com>
2022-09-17 22:15:38 +0100

    Adding font weight to headings

--- pages/[series]/index.tsx
+++ pages/[series]/index.tsx
@@ -21,8 +21,8 @@ const F1: NextPage<Props> = ({ data }) => {
 					<Accordion.Item eventKey='0'>
 						<Accordion.Header>
 							<div className='d-flex flex-column flex-sm-row w-100 align-items-center'>
-								<h4>{key}</h4>
-								<h4>{array.find((doc) => doc.weekend)?.weekend}</h4>
+								<h4 className='fw-bold'>{key}</h4>
+								<h4 className='fw-bold'>{array.find((doc) => doc.weekend)?.weekend}</h4>
 							</div>
 						</Accordion.Header>
 						<Accordion.Body className='bg-light'>
