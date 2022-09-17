7c0d32e04d3fe64e08d837354c3792035f2b7942
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-17 22:30:28 +0200

    Updating and cleaning up.
    Changing saving method of new documents. Previous method was failing to
    validate fields properly after changing from model pattern to schema pattern.

--- pages/api/forms/[data].tsx
+++ pages/api/forms/[data].tsx
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import connectMongo from '../../../lib/mongo';
-import { transformToDecOffDoc } from '../../../lib/transformToDecOffDoc';
-import multiparty from 'multiparty';
-import { readPDFPages } from '../../../lib/readPDFPages';
-import { streamToBuffer } from '../../../lib/streamToBuffer';
-import { parseFields, parseFile } from '../../../lib/multiparty';
 import { dbNameList } from '../../../lib/myData';
-import { FormContactData, FormDocData } from '../../../types/myTypes';
+import multiparty from 'multiparty';
+import { parseFields, parseFile } from '../../../lib/multiparty';
+import { streamToBuffer } from '../../../lib/streamToBuffer';
+import { readPDFPages } from '../../../lib/readPDFPages';
+import { transformToDecOffDoc } from '../../../lib/transformToDecOffDoc';
 
 export const config = {
 	api: {
@@ -29,87 +28,84 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 				} else if (series === 'formula3') {
 					seriesDB = dbNameList.f3_2022_db;
 				} else {
-					return res.status(422).json('Unsupported series');
+					return res.status(422).json('Unsupported series.');
 				}
-				if (
-					series === 'formula1' ||
-					series === 'formula2' ||
-					series === 'formula3'
-				) {
-					// Currently not working when imported, looking for fix...
-					//
-					// const file = await parseFile(req);
+				// Currently not working when imported, looking for fix...
+				// const file = await parseFile(req);
 
-					const form = new multiparty.Form();
+				const form = new multiparty.Form();
 
-					// Errors may be emitted
-					// Note that if you are listening to 'part' events, the same error may be
-					// emitted from the `form` and the `part`.
-					form.on('error', (error: any) => {
-						console.log('Error parsing form: ' + error.stack);
+				// Errors may be emitted
+				// Note that if you are listening to 'part' events, the same error may be
+				// emitted from the `form` and the `part`.
+				form.on('error', (error: any) => {
+					console.log('Error parsing form: ' + error.stack);
+				});
+
+				// Parts are emitted when parsing the form
+				form.on('part', async (part) => {
+					// You *must* act on the part by reading it
+					// NOTE: if you want to ignore it, just call "part.resume()"
+					if (part.filename === undefined) {
+						// filename is not defined when this is a field and not a file
+						// console.log('got field named ' + part.name);
+						// ignore field's content
+						part.resume();
+					}
+					if (part.filename !== undefined) {
+						// filename is defined when this is a file
+						// console.log('got file named ' + part.name);
+						// ignore file's content here
+						part.resume();
+					}
+					part.on('error', (error: any) => {
+						// decide what to do
+						console.log('got error on part ' + error);
 					});
 
-					// Parts are emitted when parsing the form
-					form.on('part', async (part) => {
-						// You *must* act on the part by reading it
-						// NOTE: if you want to ignore it, just call "part.resume()"
-						if (part.filename === undefined) {
-							// filename is not defined when this is a field and not a file
-							// console.log('got field named ' + part.name);
-							// ignore field's content
-							part.resume();
-						}
-						if (part.filename !== undefined) {
-							// filename is defined when this is a file
-							// console.log('got file named ' + part.name);
-							// ignore file's content here
-							part.resume();
-						}
-						part.on('error', (error: any) => {
-							// decide what to do
-							console.log('got error on part ' + error);
-							// reject(new Error('File error'))
+					const fileBuffer = await streamToBuffer(part);
+					const pdfData = await readPDFPages(fileBuffer);
+					const transformed = transformToDecOffDoc(
+						part.filename,
+						pdfData as any,
+						req.query.series as 'formula1' | 'formula2' | 'formula3'
+					);
+					try {
+						const conn = await connectMongo(seriesDB);
+						const docExists = await conn.models.Decision_Offence.findOne({
+							doc_type: transformed.doc_type,
+							doc_name: transformed.doc_name,
+							doc_date: transformed.doc_date,
+							penalty_type: transformed.penalty_type,
+							grand_prix: transformed.grand_prix,
 						});
-
-						const fileBuffer = await streamToBuffer(part);
-						const pdfData = await readPDFPages(fileBuffer);
-						const transformed = await transformToDecOffDoc(
-							part.filename,
-							pdfData as any,
-							req.query.series as 'formula1' | 'formula2' | 'formula3'
-						);
-						try {
-							const conn = await connectMongo(seriesDB);
-							await conn.models.Decision_Offence.findOneAndUpdate(
-								{
-									doc_type: transformed.doc_type,
-									doc_name: transformed.doc_name,
-									doc_date: transformed.doc_date,
-									penalty_type: transformed.penalty_type,
-									grand_prix: transformed.grand_prix,
-								},
-								{ $setOnInsert: { ...transformed } },
-								{ timestamps: true, upsert: true }
-							);
-							return res.status(200).json('Document saved');
-						} catch (error) {
-							console.log(error);
+						if (docExists) {
+							return res
+								.status(403)
+								.json(
+									'Document already exists. If you believe this to be mistake please use Contact form to let me know about this issue.'
+								);
 						}
-					});
+						await conn.models.Decision_Offence.create({
+							...transformed,
+							manual_upload: true,
+						});
+						return res.status(200).json('Document saved.');
+					} catch (error) {
+						console.log(error);
+						return res.status(500).json('Unknown server error.');
+					}
+				});
 
-					// Close emitted after form parsed
-					form.on('close', function () {
-						console.log('Upload completed!');
-					});
+				// Close emitted after form parsed
+				form.on('close', function () {
+					console.log('Upload completed!');
+				});
 
-					// Parse req
-					form.parse(req);
-				} else {
-					return res.status(422).json('Unsupported series');
-				}
+				// Parse req
+				form.parse(req);
 			} catch (error) {
-				console.log(error);
-				return res.status(500).json('Unknown server error');
+				return res.status(500).json('Unknown server error.');
 			}
 		}
 		if (req.query.data === 'doc-data') {
@@ -118,30 +114,30 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 				if (!fields.series && !fields.title && !fields.url) {
 					return res
 						.status(40)
-						.json('Must provide Series and at least Title or Link / URL');
+						.json('Must provide Series and at least Title or Link / URL.');
 				}
 				const conn = await connectMongo('otherDocs');
 				const newReport = new conn.models.Missing_Doc(fields);
 				await newReport.save();
-				return res.status(200).json('Document saved');
+				return res.status(200).json('Document saved.');
 			} catch (error) {
 				console.log(error);
-				return res.status(500).json('Unknown server error');
+				return res.status(500).json('Unknown server error.');
 			}
 		}
 		if (req.query.data === 'contact') {
 			try {
 				const fields = await parseFields(req);
 				if (!fields.email && !fields.message) {
-					return res.status(422).json('Must provide an Email and a Message');
+					return res.status(422).json('Must provide an Email and a Message.');
 				}
 				const conn = await connectMongo('otherDocs');
 				const newReport = new conn.models.Contact_Doc(fields);
 				await newReport.save();
-				return res.status(200).json('Document saved');
+				return res.status(200).json('Document saved.');
 			} catch (error) {
 				console.log(error);
-				return res.status(500).json('Unknown server error');
+				return res.status(500).json('Unknown server error.');
 			}
 		}
 	}
