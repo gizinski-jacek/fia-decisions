b729a385ef68b0844b1469a97f26ff32b6b7e188
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-17 17:39:23 +0200

    Adding Contact form and handlers.

---
+++ components/forms/ContactForm.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import axios, { AxiosError } from 'axios';
+import { FormContactData } from '../../types/myTypes';
+import { defaultContactData } from '../../lib/myData';
+
+const ContactForm = () => {
+	const [formData, setFormData] = useState<FormContactData>(defaultContactData);
+	const [formErrors, setFormErrors] = useState<string[]>([]);
+
+	const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFormErrors([]);
+		const { name, value } = e.target;
+		setFormData((prevState) => ({ ...prevState, [name]: value }));
+	};
+
+	const handleSubmit = async () => {
+		try {
+			if (!formData.email && !formData.message) {
+				setFormErrors(['Must provide an email and a message']);
+				return;
+			}
+			const uploadData = new FormData();
+			for (const [key, value] of Object.entries(formData)) {
+				uploadData.append(key, value);
+			}
+			await axios.post('/api/forms/contact', uploadData);
+			setFormData(defaultContactData);
+			setFormErrors([]);
+		} catch (error) {
+			if (error instanceof AxiosError) {
+				setFormErrors([error?.response?.data || 'Unknown server error']);
+			} else {
+				setFormErrors([(error as Error).message || 'Unknown server error']);
+			}
+		}
+	};
+
+	return (
+		<Form>
+			<h4>
+				Use this form to report something or contact me about any other matter.
+			</h4>
+			<Form.Group className='p-3 my-3 rounded-2 bg-light'>
+				<Form.Label htmlFor='email'>Email</Form.Label>
+				<Form.Control
+					className={`mb-2 ${
+						formErrors.length && !formData.email
+							? 'outline-error'
+							: !formData.email
+							? 'outline-warning'
+							: 'outline-success'
+					}`}
+					type='text'
+					name='email'
+					id='email'
+					maxLength={16}
+					onChange={handleInputChange}
+					value={formData.email}
+					placeholder='Email'
+				/>
+				<Form.Label htmlFor='message'>Message</Form.Label>
+				<Form.Control
+					className={`mb-2 ${
+						formErrors.length && !formData.message
+							? 'outline-error'
+							: !formData.message
+							? 'outline-warning'
+							: 'outline-success'
+					}`}
+					type='text'
+					name='message'
+					id='message'
+					maxLength={128}
+					onChange={handleInputChange}
+					value={formData.message}
+					placeholder='Message'
+				/>
+			</Form.Group>
+			{formErrors.map((message, index) => (
+				<div className='mx-1 text-danger' key={index}>
+					{message}
+				</div>
+			))}
+			<div className='pt-3 w-100 border-top text-end'>
+				<Button variant='primary' type='submit' onClick={handleSubmit}>
+					Submit
+				</Button>
+			</div>
+		</Form>
+	);
+};
+
+export default ContactForm;
