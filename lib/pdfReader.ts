39914b11d5d6123be50d9f47373c1b59a794f884
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-18 19:35:45 +0200

    Renaming file.
    Adding function handling parsing with fs

--- lib/pdfReader.ts
+++ lib/pdfReader.ts
@@ -15,3 +15,19 @@ export const readPDFPages = (buffer: Buffer): Promise<string[]> => {
 		});
 	});
 };
+
+export const readPDFPagesWithFS = (filePath: string): Promise<string[]> => {
+	const reader = new PdfReader();
+	return new Promise((resolve, reject) => {
+		const stringsArray: string[] = [];
+		reader.parseFileItems(filePath, (err: any, item: any) => {
+			if (err) {
+				reject(err);
+			} else if (!item) {
+				resolve(stringsArray);
+			} else if (item.text) {
+				stringsArray.push(item.text.normalize('NFKD'));
+			}
+		});
+	});
+};
