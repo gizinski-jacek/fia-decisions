8f23ec9f8e48c3649ba8880448fed836dc3a5062
Jacek Giziński <jacektrg+github@gmail.com>
2022-09-18 21:54:18 +0100

    Adding margin

--- pages/[series]/index.tsx
+++ pages/[series]/index.tsx
@@ -21,7 +21,7 @@ const F1: NextPage<Props> = ({ data }) => {
 					<Accordion.Item eventKey='0'>
 						<Accordion.Header>
 							<div className='d-flex flex-column me-2 flex-sm-row w-100 align-items-center'>
-								<h4 className='fw-bold'>{key}</h4>
+								<h4 className='me-sm-3 fw-bold'>{key}</h4>
 								<h4 className='fw-bold'>
 									{array.find((doc) => doc.weekend)?.weekend}
 								</h4>
