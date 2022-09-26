aa9424eb7d2bad0ffe9c2ed1f38873f8d979419f
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-27 00:44:29 +0200

    Adding other docs db name to the list.
    Adding supported series array.
    Adding default dashboard password form values

--- lib/myData.ts
+++ lib/myData.ts
@@ -4,6 +4,7 @@ import {
 	ContactFormValues,
 	DataFormValues,
 	FileFormValues,
+	DashboardFormValues,
 } from '../types/myTypes';
 
 // Main FIA domain.
@@ -19,28 +20,35 @@ export const fiaPageList: FiaPageList = {
 		'https://www.fia.com/documents/championships/fia-formula-3-championship-1012/season/season-2022-2005',
 };
 
+export const supportedSeries: string[] = ['formula1', 'formula2', 'formula3'];
+
 // Database names for each supported racing series.
 export const dbNameList: DatabaseNameList = {
 	f1_2022_db: '2022_F1_WC_Docs',
 	f2_2022_db: '2022_F2_WC_Docs',
 	f3_2022_db: '2022_F3_WC_Docs',
+	other_documents_db: 'otherDocs',
 };
 
 // Default form data state used in FileForm component.
-export const defaultFileData: FileFormValues = {
+export const defaultFileValues: FileFormValues = {
 	series: '',
 	file: null,
 };
 
 // Default form data state used in DataForm component.
-export const defaultDocData: DataFormValues = {
+export const defaultDocValues: DataFormValues = {
 	series: '',
 	title: '',
 	url: '',
 };
 
 // Default form data state used in ContactForm component.
-export const defaultContactData: ContactFormValues = {
+export const defaultContactValues: ContactFormValues = {
 	email: '',
 	message: '',
+};
+
+export const defaultDashboardValues: DashboardFormValues = {
+	password: '',
 };
