1ffeb5ecea5f0c1881b7455a0abced276fa0727a
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-27 23:21:24 +0200

    Updating after changes to Mssing Doc model.

--- components/forms/DataForm.tsx
+++ components/forms/DataForm.tsx
@@ -2,11 +2,13 @@ import { useState, useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import axios, { AxiosError } from 'axios';
 import { DataFormValues } from '../../types/myTypes';
-import { defaultDocValues } from '../../lib/myData';
+import { defaultDataFormValues, supportedSeries } from '../../lib/myData';
 import LoadingBar from '../LoadingBar';
 
 const DataForm = () => {
-	const [formData, setFormData] = useState<DataFormValues>(defaultDocValues);
+	const [formData, setFormData] = useState<DataFormValues>(
+		defaultDataFormValues
+	);
 	const [formErrors, setFormErrors] = useState<string[]>([]);
 	const [sending, setSending] = useState(false);
 	const [submitSuccess, setSubmitSuccess] = useState(false);
@@ -23,19 +25,18 @@ const DataForm = () => {
 		setSubmitSuccess(false);
 		setFormErrors([]);
 		try {
-			if (!formData.series || (!formData.title && !formData.url)) {
-				setFormErrors([
-					'Must choose a Series and provide at least a Title or a Link / URL.',
-				]);
+			if (!formData.series || !formData.description) {
+				setFormErrors(['Must choose a Series and provide a Description.']);
 				return;
 			}
 			const uploadData = new FormData();
+
 			for (const [key, value] of Object.entries(formData)) {
 				uploadData.append(key, value);
 			}
 			setSending(true);
 			await axios.post('/api/forms/data', uploadData, { timeout: 15000 });
-			setFormData(defaultDocValues);
+			setFormData(defaultDataFormValues);
 			formRef.current?.reset();
 			setSubmitSuccess(true);
 			setSending(false);
@@ -55,8 +56,8 @@ const DataForm = () => {
 	return (
 		<Form ref={formRef}>
 			<h4>
-				Use this form to provide data about a penalty You believe is missing
-				from the list.
+				Use this form to provide information about a penalty You believe is
+				missing from the list.
 			</h4>
 			<div className='p-3 my-3 rounded-2 bg-light'>
 				<Form.Group className='mb-3'>
@@ -79,105 +80,68 @@ const DataForm = () => {
 						required
 					>
 						<option value=''>Choose Formula series</option>
-						<option value='formula1'>Formula 1</option>
-						<option value='formula2'>Formula 2</option>
-						<option value='formula3'>Formula 3</option>
+						{supportedSeries.map((s, i) => (
+							<option key={i} value={s} className='text-capitalize'>
+								{s.replace('_', ' ')}
+							</option>
+						))}
 					</Form.Select>
 				</Form.Group>
-				<Form.Group className='mb-3'>
+				<Form.Group>
 					<Form.Label htmlFor='title' className='fw-bolder'>
-						Title
+						Description or Link
 					</Form.Label>
 					<Form.Control
 						as='textarea'
 						className={
-							formErrors.length > 0 && !formData.title && !formData.url
+							formErrors.length > 0 && !formData.description
 								? 'outline-error'
-								: (!formData.title && !formData.url) ||
-								  (!formData.url &&
-										formData.title &&
-										(formData.title.length < 8 ||
-											formData.title.length > 256)) ||
-								  (!formData.title &&
-										formData.url &&
-										(formData.url.length < 16 || formData.url.length > 256))
+								: !formData.description ||
+								  formData.description.length < 16 ||
+								  formData.description.length > 256
 								? 'outline-warning'
 								: 'outline-success'
 						}
 						type='text'
-						name='title'
-						id='title'
+						name='description'
+						id='description'
 						minLength={8}
 						maxLength={256}
-						rows={formData.title && formData.title.length <= 128 ? 4 : 7}
+						rows={formData.description.length <= 128 ? 5 : 7}
 						onChange={handleInputChange}
-						value={formData.title}
-						placeholder='Title'
+						value={formData.description}
+						placeholder='Description or Link'
 						disabled={sending}
-						required={(formData.url && formData.url?.length > 0) || true}
-						aria-describedby='titleInputHelpText'
+						required
+						aria-describedby='descriptionInputHelpText'
 					/>
-					<Form.Text muted id='titleInputHelpText'>
-						Title 8-256 characters long
+					<Form.Text muted id='descriptionInputHelpText'>
+						16-256 characters long
 					</Form.Text>
 				</Form.Group>
-				<Form.Group>
-					<Form.Label htmlFor='url' className='fw-bolder'>
-						Link / URL
-					</Form.Label>
-					<Form.Control
-						className={
-							formErrors.length > 0 && !formData.title && !formData.url
-								? 'outline-error'
-								: (!formData.title && !formData.url) ||
-								  (!formData.title &&
-										formData.url &&
-										(formData.url.length < 16 || formData.url.length > 256)) ||
-								  (!formData.url &&
-										formData.title &&
-										(formData.title.length < 8 || formData.title.length > 256))
-								? 'outline-warning'
-								: 'outline-success'
-						}
-						type='text'
-						name='url'
-						id='url'
-						minLength={16}
-						maxLength={256}
-						onChange={handleInputChange}
-						value={formData.url}
-						placeholder='Link / URL'
-						disabled={sending}
-						required={(formData.title && formData.title?.length > 0) || true}
-						aria-describedby='urlInputHelpText'
-					/>
-					<Form.Text muted id='urlInputHelpText'>
-						Valid Link / URL, 16-256 characters long
-					</Form.Text>
-					{formErrors.length > 0 && (
-						<div className='m-0 mt-4 alert alert-danger alert-dismissible'>
-							{formErrors.map((message, index) => (
-								<div key={index}>{message}</div>
-							))}
-							<button
-								type='button'
-								className='btn btn-close'
-								onClick={() => setFormErrors([])}
-							></button>
-						</div>
-					)}
-					{submitSuccess && (
-						<div className='m-0 mt-4 alert alert-success alert-dismissible'>
-							<strong>Form submitted successfully!</strong>
-							<button
-								type='button'
-								className='btn btn-close'
-								onClick={() => setSubmitSuccess(false)}
-							></button>
-						</div>
-					)}
-					{sending && <LoadingBar />}
-				</Form.Group>
+				{formErrors.length > 0 && (
+					<div className='m-0 mt-4 alert alert-danger alert-dismissible'>
+						{formErrors.map((message, index) => (
+							<div key={index}>{message}</div>
+						))}
+						<button
+							type='button'
+							className='btn btn-close'
+							onClick={() => setFormErrors([])}
+						></button>
+					</div>
+				)}
+				{submitSuccess && (
+					<div className='m-0 mt-4 alert alert-success alert-dismissible'>
+						<strong>Form submitted successfully!</strong>
+						<button
+							type='button'
+							className='btn btn-close'
+							onClick={() => setSubmitSuccess(false)}
+						></button>
+					</div>
+				)}
+				{sending && <LoadingBar />}
 			</div>
 			<div className='w-100 text-end'>
 				<Button
