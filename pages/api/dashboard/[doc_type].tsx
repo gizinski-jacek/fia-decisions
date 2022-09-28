5fb8dc6d83acd724fd6d62b53b8d50123b75c1be
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-28 23:50:42 +0200

    Extracting token validation logic.
    Adding support for DELETE method.
    Updating responses and error types.
    Cleaning up.

--- pages/api/dashboard/[doc_type].tsx
+++ pages/api/dashboard/[doc_type].tsx
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
-import jwt from 'jsonwebtoken';
 import connectMongo from '../../../lib/mongo';
 import { dbNameList } from '../../../lib/myData';
 import {
@@ -9,6 +7,7 @@ import {
 	DecisionOffenceModel,
 	MissingDocModel,
 } from '../../../types/myTypes';
+import { verifyToken } from '../../../lib/utils';
 
 const handler = async (
 	req: NextApiRequest,
@@ -42,168 +41,133 @@ const handler = async (
 		// 			};
 		// 		}
 		// 		const conn = await connectMongo(dbNameList.other_documents_db);
-		// 		const document_list = await conn.models.Missing_Doc.find({}).exec();
+		// 		const document_list = await conn.models.Decision_Offence.find({}).exec();
 		// 		return res.status(200).json(document_list);
-		// 	} catch (error) {
-		// 		return res.status(404).json('Unknown server error.');
+		// 	} catch (error: any) {
+		// 		return res.status(404).json('Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.');
 		// 	}
 		// }
+		//
 		if (doc_type === 'missing') {
 			try {
-				if (!process.env.JWT_STRATEGY_SECRET) {
-					throw new Error(
-						'Please define JWT_STRATEGY_SECRET environment variable inside .env.local'
-					);
-				}
-				const { token } = req.cookies;
-				if (!token) {
-					return res.redirect('/dashboard');
-				}
-				const decodedToken = jwt.verify(token, process.env.JWT_STRATEGY_SECRET);
-				if (decodedToken !== process.env.PAYLOAD_STRING) {
+				const tokenValid = verifyToken(req);
+				if (!tokenValid) {
 					res.setHeader(
 						'Set-Cookie',
 						`token=; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=0`
 					);
-					return res.redirect('/dashboard');
+					return res.status(401).end();
 				}
 				const conn = await connectMongo(dbNameList.other_documents_db);
 				const document_list = await conn.models.Missing_Doc.find({}).exec();
 				return res.status(200).json(document_list);
-			} catch (error) {
-				return res.status(404).json('Unknown server error.');
+			} catch (error: any) {
+				console.log(error);
+				return res
+					.status(404)
+					.json(
+						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
+					);
 			}
 		}
 		if (doc_type === 'contact') {
 			try {
-				if (!process.env.JWT_STRATEGY_SECRET) {
-					throw new Error(
-						'Please define JWT_STRATEGY_SECRET environment variable inside .env.local'
-					);
-				}
-				const { token } = req.cookies;
-				if (!token) {
-					return res.redirect('/dashboard');
-				}
-				const decodedToken = jwt.verify(token, process.env.JWT_STRATEGY_SECRET);
-				if (decodedToken !== process.env.PAYLOAD_STRING) {
+				const tokenValid = verifyToken(req);
+				if (!tokenValid) {
 					res.setHeader(
 						'Set-Cookie',
 						`token=; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=0`
 					);
-					return res.redirect('/dashboard');
+					return res.status(401).end();
 				}
 				const conn = await connectMongo(dbNameList.other_documents_db);
 				const document_list = await conn.models.Contact_Doc.find({}).exec();
 				return res.status(200).json(document_list);
-			} catch (error) {
-				return res.status(404).json('Unknown server error.');
+			} catch (error: any) {
+				return res
+					.status(404)
+					.json(
+						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
+					);
 			}
 		}
+	} else if (req.method === 'DELETE') {
+		const { doc_type, doc_id } = req.query;
+		// if (doc_type === 'penalty') {
+		// 	try {
+		// 		const tokenValid =  verifyToken(req);
+		// 		if (!tokenValid) {
+		// 			res.setHeader(
+		// 				'Set-Cookie',
+		// 				`token=; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=0`
+		// 			);
+		// 			return res.status(401).end();
+		// 		}
+		// 		if (!doc_id) {
+		// 			return res.status(403).json('Document Id is required.');
+		// 		}
+		// 		const conn = await connectMongo(dbNameList.other_documents_db);
+		// 		await conn.models.Decision_Offence.findByIdAndDelete(doc_id).exec();
+		// 		return res.status(200).end();
+		// 	} catch (error: any) {
+		// 		return res
+		// 			.status(404)
+		// 			.json(
+		// 				'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
+		// 			);
+		// 	}
+		// }
+		if (doc_type === 'missing') {
+			try {
+				const tokenValid = verifyToken(req);
+				if (!tokenValid) {
+					res.setHeader(
+						'Set-Cookie',
+						`token=; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=0`
+					);
+					return res.status(401).end();
+				}
+				if (!doc_id) {
+					return res.status(403).json('Document Id is required.');
+				}
+				const conn = await connectMongo(dbNameList.other_documents_db);
+				await conn.models.Missing_Doc.findByIdAndDelete(doc_id).exec();
+				return res.status(200).end();
+			} catch (error: any) {
+				return res
+					.status(404)
+					.json(
+						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
+					);
+			}
+		}
+		if (doc_type === 'contact') {
+			try {
+				const tokenValid = verifyToken(req);
+				if (!tokenValid) {
+					res.setHeader(
+						'Set-Cookie',
+						`token=; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=0`
+					);
+					return res.status(401).end();
+				}
+				if (!doc_id) {
+					return res.status(403).json('Document Id is required.');
+				}
+				const conn = await connectMongo(dbNameList.other_documents_db);
+				await conn.models.Contact_Doc.findByIdAndDelete(doc_id).exec();
+				return res.status(200).end();
+			} catch (error: any) {
+				return res
+					.status(404)
+					.json(
+						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
+					);
+			}
+		}
+	} else {
+		return res.status(405).end();
 	}
-	// if (req.method === 'DELETE') {
-	// 	const { doc_type, doc_id } = req.query;
-	// 	if (doc_type === 'penalty') {
-	// 		try {
-	// 			const { authorization } = req.headers;
-	// 			if (authorization === `Bearer ${process.env.CRON_JOB_CLEANUP_SECRET}`) {
-	// 				const files = fs.readdirSync('./pdfreader/');
-
-	// 				if (files.length === 0) {
-	// 					return res.status(200).json('No files to clean up.');
-	// 				}
-
-	// 				await Promise.all(
-	// 					files.map(
-	// 						(file) =>
-	// 							new Promise(async (resolve, reject) => {
-	// 								fs.unlink('./pdfreader/' + file, (error) => {
-	// 									if (error) {
-	// 										reject(error);
-	// 									}
-	// 								});
-	// 								resolve(null);
-	// 							})
-	// 					)
-	// 				);
-
-	// 				console.log('Finished cleanup.');
-	// 				return res.status(200).json('Finished cleanup.');
-	// 			} else {
-	// 				return res.status(401);
-	// 			}
-	// 		} catch (error) {
-	// 			return res.status(405).json('Job not supported.');
-	// 		}
-	// 	}
-	// 	if (doc_type === 'missing') {
-	// 		try {
-	// 			const { authorization } = req.headers;
-	// 			if (authorization === `Bearer ${process.env.CRON_JOB_CLEANUP_SECRET}`) {
-	// 				const files = fs.readdirSync('./pdfreader/');
-
-	// 				if (files.length === 0) {
-	// 					return res.status(200).json('No files to clean up.');
-	// 				}
-
-	// 				await Promise.all(
-	// 					files.map(
-	// 						(file) =>
-	// 							new Promise(async (resolve, reject) => {
-	// 								fs.unlink('./pdfreader/' + file, (error) => {
-	// 									if (error) {
-	// 										reject(error);
-	// 									}
-	// 								});
-	// 								resolve(null);
-	// 							})
-	// 					)
-	// 				);
-
-	// 				console.log('Finished cleanup.');
-	// 				return res.status(200).json('Finished cleanup.');
-	// 			} else {
-	// 				return res.status(401);
-	// 			}
-	// 		} catch (error) {
-	// 			return res.status(405).json('Job not supported.');
-	// 		}
-	// 	}
-	// 	if (doc_type === 'contact') {
-	// 		try {
-	// 			const { authorization } = req.headers;
-	// 			if (authorization === `Bearer ${process.env.CRON_JOB_CLEANUP_SECRET}`) {
-	// 				const files = fs.readdirSync('./pdfreader/');
-
-	// 				if (files.length === 0) {
-	// 					return res.status(200).json('No files to clean up.');
-	// 				}
-
-	// 				await Promise.all(
-	// 					files.map(
-	// 						(file) =>
-	// 							new Promise(async (resolve, reject) => {
-	// 								fs.unlink('./pdfreader/' + file, (error) => {
-	// 									if (error) {
-	// 										reject(error);
-	// 									}
-	// 								});
-	// 								resolve(null);
-	// 							})
-	// 					)
-	// 				);
-
-	// 				console.log('Finished cleanup.');
-	// 				return res.status(200).json('Finished cleanup.');
-	// 			} else {
-	// 				return res.status(401);
-	// 			}
-	// 		} catch (error) {
-	// 			return res.status(405).json('Job not supported.');
-	// 		}
-	// 	}
-	// }
-	return res.status(405).json('Method not supported.');
 };
 
 export default handler;
