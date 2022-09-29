d7520387910f17acb55fd05991c9ecf5ebb6cfe9
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-30 00:15:49 +0200

    Adding option to pass custom width.

--- components/LoadingBar.tsx
+++ components/LoadingBar.tsx
@@ -1,10 +1,13 @@
 import style from '../styles/LoadingBar.module.scss';
 
-const LoadingBar: React.FC<{ margin?: string }> = ({ margin }) => {
+const LoadingBar: React.FC<{ margin?: string; width?: string }> = ({
+	margin,
+	width,
+}) => {
 	return (
 		<div
 			className={style.loading_bar_container}
-			style={{ margin: margin || '' }}
+			style={{ margin: margin || '', width: width || '' }}
 		>
 			<div className={style.loading_bar}></div>
 		</div>
