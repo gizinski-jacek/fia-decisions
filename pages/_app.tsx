b617af2b76790269c086788061de2a08c581fe93
Jacek Gizinski <jacektrg+github@gmail.com>
2022-09-30 00:20:28 +0200

    Adding LoadingBar.
    Rendering loading bar when navigating between pages and before gSSP
    fetches and returns data.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,37 @@ import '../styles/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
+import LoadingBar from '../components/LoadingBar';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const start = () => {
+			setLoading(true);
+		};
+		const end = () => {
+			setLoading(false);
+		};
+		Router.events.on('routeChangeStart', start);
+		Router.events.on('routeChangeComplete', end);
+		Router.events.on('routeChangeError', end);
+		return () => {
+			Router.events.off('routeChangeStart', start);
+			Router.events.off('routeChangeComplete', end);
+			Router.events.off('routeChangeError', end);
+		};
+	}, []);
+
 	return (
 		<Layout>
-			<Component {...pageProps} />
+			{loading ? (
+				<LoadingBar margin='5rem 10rem' />
+			) : (
+				<Component {...pageProps} />
+			)}
 		</Layout>
 	);
 };
