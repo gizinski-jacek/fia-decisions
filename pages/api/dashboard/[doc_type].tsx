04a5c97842ada4a8943a9cbf7f869fcaa03646cf
Jacek Gizinski <jacektrg+github@gmail.com>
2022-10-01 15:55:27 +0200

    Adding PUT method handler.
    Handles accepting document into main db.
    Finds doc by id, checks for duplicate in main db, deletes from
    Other_Docs db, sets isNew to true and generates new ObjectId, saves in
    main db.
    Updating response messages.

--- pages/api/dashboard/[doc_type].tsx
+++ pages/api/dashboard/[doc_type].tsx
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import connectMongo from '../../../lib/mongo';
+import mongoose from 'mongoose';
 import { dbNameList } from '../../../lib/myData';
 import {
 	ContactDocModel,
@@ -60,9 +61,7 @@ const handler = async (
 			} catch (error: any) {
 				return res
 					.status(404)
-					.json(
-						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
-					);
+					.json('Unknown server error. Failed to get documents.');
 			}
 		}
 		if (docTypeStrings[0] === 'missing-info') {
@@ -81,9 +80,7 @@ const handler = async (
 			} catch (error: any) {
 				return res
 					.status(404)
-					.json(
-						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
-					);
+					.json('Unknown server error. Failed to get documents.');
 			}
 		}
 		if (docTypeStrings[0] === 'contact-message') {
@@ -102,9 +99,7 @@ const handler = async (
 			} catch (error: any) {
 				return res
 					.status(404)
-					.json(
-						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
-					);
+					.json('Unknown server error. Failed to get documents.');
 			}
 		}
 	} else if (req.method === 'DELETE') {
@@ -144,9 +139,7 @@ const handler = async (
 			} catch (error: any) {
 				return res
 					.status(404)
-					.json(
-						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
-					);
+					.json('Unknown server error. File was not deleted.');
 			}
 		}
 		if (docTypeStrings[0] === 'missing-info') {
@@ -168,9 +161,7 @@ const handler = async (
 			} catch (error: any) {
 				return res
 					.status(404)
-					.json(
-						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
-					);
+					.json('Unknown server error. File was not deleted.');
 			}
 		}
 		if (docTypeStrings[0] === 'contact-message') {
@@ -192,10 +183,64 @@ const handler = async (
 			} catch (error: any) {
 				return res
 					.status(404)
-					.json(
-						'Unknown server error. If it is a reoccuring error, please use the Contact form to report this issue.'
-					);
+					.json('Unknown server error. File was not deleted.');
 			}
+		}
+	} else if (req.method === 'PUT') {
+		const { doc_type: series, doc_id } = req.query as {
+			doc_type: string;
+			doc_id: string;
+		};
+		let seriesDB = '';
+		if (series === 'formula1') {
+			seriesDB = dbNameList.f1_2022_db;
+		} else if (series === 'formula2') {
+			seriesDB = dbNameList.f2_2022_db;
+		} else if (series === 'formula3') {
+			seriesDB = dbNameList.f3_2022_db;
+		} else {
+			return res.status(422).json('Series is not supported.');
+		}
+		try {
+			const tokenValid = verifyToken(req);
+			if (!tokenValid) {
+				res.setHeader(
+					'Set-Cookie',
+					`token=; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=0`
+				);
+				return res.status(401).end();
+			}
+			if (!doc_id) {
+				return res.status(403).json('Document Id is required.');
+			}
+			const connOtherDB = await connectMongo(dbNameList.other_documents_db);
+			const connSeriesDB = await connectMongo(seriesDB);
+			const document = await connOtherDB.models.Decision_Offence.findById(
+				doc_id
+			).exec();
+			const docExists = await connSeriesDB.models.Decision_Offence.findOne({
+				series: series,
+				doc_type: document.doc_type,
+				doc_name: document.doc_name,
+				doc_date: document.doc_date,
+				penalty_type: document.penalty_type,
+				grand_prix: document.grand_prix,
+				weekend: document.weekend,
+			});
+			if (docExists) {
+				return res.status(403).json('Document already exists.');
+			}
+			await connOtherDB.models.Decision_Offence.findByIdAndDelete(
+				doc_id
+			).exec();
+			document._id = new mongoose.Types.ObjectId();
+			document.isNew = true;
+			await connSeriesDB.models.Decision_Offence.insertMany(document);
+			return res.status(200).end();
+		} catch (error: any) {
+			return res
+				.status(404)
+				.json('Unknown server error. Document was not accepted.');
 		}
 	} else {
 		return res.status(405).end();
