afbb2050ea2088ba9548ed56dfd80d44653e16fa
Jacek Gizinski <jacektrg+github@gmail.com>
2022-10-04 16:13:01 +0200

    Adding support for stop and go penalty

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -188,7 +188,6 @@ export const formatPenalty = (type: string, string: string): string => {
 			nineteen: '19',
 			twenty: '20',
 		};
-
 		let replacedWordWithNumber = '';
 		for (const [key, value] of Object.entries(numberWords)) {
 			if (string.toLowerCase().includes(key)) {
@@ -216,6 +215,50 @@ export const formatPenalty = (type: string, string: string): string => {
 		const matchLapReinstated = replacedWordWithNumber.match(/reinstate/im);
 		if (matchLapReinstated) {
 			return 'Lap Reinstated';
+		}
+	}
+	if (type === 'stop and go') {
+		const numberWords = {
+			one: '1',
+			two: '2',
+			three: '3',
+			four: '4',
+			five: '5',
+			six: '6',
+			seven: '7',
+			eight: '8',
+			nine: '9',
+			ten: '10',
+			eleven: '11',
+			twelve: '12',
+			thirteen: '13',
+			fourteen: '14',
+			fifteen: '15',
+			sixteen: '16',
+			seventeen: '17',
+			eighteen: '18',
+			nineteen: '19',
+			twenty: '20',
+		};
+		let replacedWordWithNumber = '';
+		for (const [key, value] of Object.entries(numberWords)) {
+			if (string.toLowerCase().includes(key)) {
+				replacedWordWithNumber = string.toLowerCase().replace(key, value);
+				break;
+			}
+		}
+		if (!replacedWordWithNumber) {
+			replacedWordWithNumber = string;
+		}
+		const matchTimeNumber = replacedWordWithNumber.match(
+			/\d{1,2}.{1,8}second?\)?/im
+		);
+		if (matchTimeNumber) {
+			return `+ ${matchTimeNumber[0]
+				.toLowerCase()
+				.replace(/\(\d{1,2}\)/im, '')
+				.replace('  ', ' ')
+				.replace(' second', 's')} ${type}`;
 		}
 	}
 	if (type === 'fine') {
