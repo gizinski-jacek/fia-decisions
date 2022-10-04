107ad9b4ffe965c80ee5566477fa0fc5c124c9b4
Jacek Gizinski <jacektrg+github@gmail.com>
2022-10-04 20:43:40 +0200

    Refactoring destructuring of parameters.

--- pages/api/dashboard/[...params].tsx
+++ pages/api/dashboard/[...params].tsx
@@ -21,26 +21,26 @@ const handler = async (
 	if (tokenValid) {
 		if (req.method === 'GET') {
 			const { params } = req.query as { params: string[] };
-			const docTypeStrings = params[0].split('__');
-			if (docTypeStrings[0] === 'penalties') {
-				let seriesDB = '';
-				if (docTypeStrings[1] === 'f1') {
-					seriesDB = dbNameList.f1_2022_db;
-				} else if (docTypeStrings[1] === 'f2') {
-					seriesDB = dbNameList.f2_2022_db;
-				} else if (docTypeStrings[1] === 'f3') {
-					seriesDB = dbNameList.f3_2022_db;
-				} else if (docTypeStrings[1] === 'missing-file') {
-					seriesDB = dbNameList.other_documents_db;
+			const docType = params[0];
+			const series = params[1];
+			const year = params[2] || new Date().getFullYear().toString();
+			const manualUpload = params[3];
+			if (docType === 'penalties') {
+				if (!series) {
+					return res.status(422).json('Unsupported series.');
+				}
+				let seriesYearDB: string;
+				if (series === 'missing-file') {
+					seriesYearDB = dbNameList.other_documents_db;
 				} else {
-					return res.status(422).json('Series is not supported.');
+					seriesYearDB = dbNameList[`${series}_${year}_db`];
+				}
+				if (!seriesYearDB) {
+					return res.status(422).json('Unsupported year.');
 				}
 				try {
-					const conn = await connectMongo(seriesDB);
-					const query =
-						docTypeStrings[2] === 'manual-upload'
-							? { manual_upload: true }
-							: {};
+					const conn = await connectMongo(seriesYearDB);
+					const query = manualUpload ? { manual_upload: true } : {};
 					const document_list: DecisionOffenceModel[] =
 						await conn.models.Decision_Offence.find(query)
 							.sort({ createdAt: -1 })
@@ -60,7 +60,7 @@ const handler = async (
 						.json('Unknown server error. Failed to get documents.');
 				}
 			}
-			if (docTypeStrings[0] === 'missing-info') {
+			if (docType === 'missing-info') {
 				try {
 					const conn = await connectMongo(dbNameList.other_documents_db);
 					const document_list: MissingDocModel[] =
@@ -72,7 +72,7 @@ const handler = async (
 						.json('Unknown server error. Failed to get documents.');
 				}
 			}
-			if (docTypeStrings[0] === 'contact-message') {
+			if (docType === 'contact-message') {
 				try {
 					const conn = await connectMongo(dbNameList.other_documents_db);
 					const document_list: ContactDocModel[] =
@@ -84,30 +84,30 @@ const handler = async (
 						.json('Unknown server error. Failed to get documents.');
 				}
 			}
+		} else if (req.method === 'POST') {
+			//
+			//	Create new doc from manually inserted data.
+			//
 		} else if (req.method === 'DELETE') {
 			const { params } = req.query as { params: string[] };
-			if (!params[1]) {
+			const docType = params[0];
+			const series = supportedSeries.find((s) => s === params[1].toLowerCase());
+			const year = params[2];
+			const docId = params[3];
+			if (docId) {
 				return res.status(403).json('Document Id is required.');
 			}
-			const docTypeStrings = params[0].split('__');
-			if (docTypeStrings[0] === 'penalties') {
-				let seriesDB = '';
-				if (docTypeStrings[1] === 'f1') {
-					seriesDB = dbNameList.f1_2022_db;
-				} else if (docTypeStrings[1] === 'f2') {
-					seriesDB = dbNameList.f2_2022_db;
-				} else if (docTypeStrings[1] === 'f3') {
-					seriesDB = dbNameList.f3_2022_db;
-				} else if (docTypeStrings[1] === 'missing-file') {
-					seriesDB = dbNameList.other_documents_db;
-				} else {
-					return res.status(422).json('Series is not supported.');
+			if (docType === 'penalties') {
+				if (!series) {
+					return res.status(422).json('Unsupported series.');
+				}
+				const seriesYearDB = dbNameList[`${series}_${year}_db`];
+				if (!seriesYearDB) {
+					return res.status(422).json('Unsupported year.');
 				}
 				try {
-					const conn = await connectMongo(seriesDB);
-					await conn.models.Decision_Offence.findByIdAndDelete(
-						params[1]
-					).exec();
+					const conn = await connectMongo(seriesYearDB);
+					await conn.models.Decision_Offence.findByIdAndDelete(docId).exec();
 					return res.status(200).end();
 				} catch (error: any) {
 					return res
@@ -115,10 +115,10 @@ const handler = async (
 						.json('Unknown server error. File was not deleted.');
 				}
 			}
-			if (docTypeStrings[0] === 'missing-info') {
+			if (docType === 'missing-info') {
 				try {
 					const conn = await connectMongo(dbNameList.other_documents_db);
-					await conn.models.Missing_Doc.findByIdAndDelete(params[1]).exec();
+					await conn.models.Missing_Doc.findByIdAndDelete(docId).exec();
 					return res.status(200).end();
 				} catch (error: any) {
 					return res
@@ -126,10 +126,10 @@ const handler = async (
 						.json('Unknown server error. File was not deleted.');
 				}
 			}
-			if (docTypeStrings[0] === 'contact-message') {
+			if (docType === 'contact-message') {
 				try {
 					const conn = await connectMongo(dbNameList.other_documents_db);
-					await conn.models.Contact_Doc.findByIdAndDelete(params[1]).exec();
+					await conn.models.Contact_Doc.findByIdAndDelete(docId).exec();
 					return res.status(200).end();
 				} catch (error: any) {
 					return res
@@ -139,45 +139,46 @@ const handler = async (
 			}
 		} else if (req.method === 'PUT') {
 			const { params } = req.query as { params: string[] };
-			if (!params[1]) {
-				return res.status(403).json('Document Id is required.');
-			}
-			try {
-				if (!supportedSeries.some((s) => s === params[0])) {
+			const [updateType, series, docId] = params;
+			if (updateType === 'accept-document') {
+				if (!docId) {
+					return res.status(403).json('Document Id is required.');
+				}
+				if (!supportedSeries.some((s) => s === series)) {
 					return res.status(422).json('Series is not supported.');
 				}
-				const connOtherDB = await connectMongo(dbNameList.other_documents_db);
-				const document = await connOtherDB.models.Decision_Offence.findById(
-					params[1]
-				).exec();
-
-				const docYear = new Date(document.doc_date).getFullYear().toString();
-				const seriesDB = dbNameList[`${params[0]}_${docYear}_db`];
-
-				const connSeriesDB = await connectMongo(seriesDB);
-				const docExists = await connSeriesDB.models.Decision_Offence.findOne({
-					series: params[0],
-					doc_type: document.doc_type,
-					doc_name: document.doc_name,
-					doc_date: document.doc_date,
-					penalty_type: document.penalty_type,
-					grand_prix: document.grand_prix,
-					weekend: document.weekend,
-				});
-				if (docExists) {
-					return res.status(403).json('Document already exists.');
+				try {
+					const connOtherDB = await connectMongo(dbNameList.other_documents_db);
+					const document = await connOtherDB.models.Decision_Offence.findById(
+						docId
+					).exec();
+					const docYear = new Date(document.doc_date).getFullYear().toString();
+					const seriesDB = dbNameList[`${series}_${docYear}_db`];
+					const connSeriesDB = await connectMongo(seriesDB);
+					const docExists = await connSeriesDB.models.Decision_Offence.findOne({
+						series: series,
+						doc_type: document.doc_type,
+						doc_name: document.doc_name,
+						doc_date: document.doc_date,
+						penalty_type: document.penalty_type,
+						grand_prix: document.grand_prix,
+						weekend: document.weekend,
+					});
+					if (docExists) {
+						return res.status(403).json('Document already exists.');
+					}
+					await connOtherDB.models.Decision_Offence.findByIdAndDelete(
+						docId
+					).exec();
+					document._id = new mongoose.Types.ObjectId();
+					document.isNew = true;
+					await connSeriesDB.models.Decision_Offence.insertMany(document);
+					return res.status(200).end();
+				} catch (error: any) {
+					return res
+						.status(404)
+						.json('Unknown server error. Document was not accepted.');
 				}
-				await connOtherDB.models.Decision_Offence.findByIdAndDelete(
-					params[1]
-				).exec();
-				document._id = new mongoose.Types.ObjectId();
-				document.isNew = true;
-				await connSeriesDB.models.Decision_Offence.insertMany(document);
-				return res.status(200).end();
-			} catch (error: any) {
-				return res
-					.status(404)
-					.json('Unknown server error. Document was not accepted.');
 			}
 		} else {
 			return res.status(405).end();
