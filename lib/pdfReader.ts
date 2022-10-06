3759a2d95e9cb1e80d8302d4ea177453d5786511
Jacek Gizinski <jacektrg+github@gmail.com>
2022-10-06 19:45:12 +0200

    Removing function updating with host file system.

--- lib/pdfReader.ts
+++ lib/pdfReader.ts
@@ -16,20 +16,3 @@ export const readPDFPages = (buffer: Buffer): Promise<string[]> => {
 		});
 	});
 };
-
-// Parsing PDF files from file system and returning document strings in array.
-export const readPDFPagesWithFS = (filePath: string): Promise<string[]> => {
-	const reader = new PdfReader();
-	return new Promise((resolve, reject) => {
-		const stringsArray: string[] = [];
-		reader.parseFileItems(filePath, (err: any, item: any) => {
-			if (err) {
-				reject(err);
-			} else if (!item) {
-				resolve(stringsArray);
-			} else if (item.text) {
-				stringsArray.push(item.text.normalize('NFKD'));
-			}
-		});
-	});
-};
