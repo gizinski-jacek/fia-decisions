587e66361a220ef6588186c8e80578992bbaf245
Jacek Gizinski <jacektrg+github@gmail.com>
2022-10-13 17:45:57 +0200

    Cleaning up theme context provider

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { DrawerContextProvider } from '../hooks/DrawerProvider';
-import { ThemeContextProvider } from '../hooks/ThemeProvider';
 import Drawer from './Drawer';
 import ScrollToTopBtn from './ScrollToTopBtn';
 
@@ -10,20 +9,18 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
 	return (
-		<ThemeContextProvider>
-			<DrawerContextProvider>
-				<Head>
-					<title>Track Limits</title>
-					<meta
-						name='description'
-						content='Quickly find out which Formula drivers got hit by penalties.'
-					/>
-					<link rel='icon' href='/favicon.ico' />
-				</Head>
-				<Drawer>{children}</Drawer>
-				<ScrollToTopBtn />
-			</DrawerContextProvider>
-		</ThemeContextProvider>
+		<DrawerContextProvider>
+			<Head>
+				<title>Track Limits</title>
+				<meta
+					name='description'
+					content='Quickly find out which Formula drivers got hit by penalties.'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Drawer>{children}</Drawer>
+			<ScrollToTopBtn />
+		</DrawerContextProvider>
 	);
 };
 
