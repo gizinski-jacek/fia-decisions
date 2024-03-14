62d2166ddb2194c1d214c226cbf8e10dc8244c8b
Jacek Gizinski <jacektrg+github@gmail.com>
2024-03-14 18:39:34 +0100

    Renaming/cleaning up declarations names and types

--- lib/myData.ts
+++ lib/myData.ts
@@ -1,10 +1,10 @@
 import {
 	ContactFormValues,
-	DataFormValues,
+	InformationFormValues,
 	FileFormValues,
 	LoginFormValues,
 	SeriesDataFormValues,
-	UpdateDocsFormValues,
+	UpdatePenaltiesFormValues as UpdatePenaltiesFormValues,
 } from '../types/myTypes';
 
 // Main FIA domain.
@@ -13,29 +13,23 @@ export const fiaDomain: string = 'https://www.fia.com';
 // List of supported Formula Series.
 export const supportedSeries: string[] = ['f1', 'f2', 'f3'];
 
+// Default form data state used in ContactForm component.
+export const defaultContactFormValues: ContactFormValues = {
+	email: '',
+	message: '',
+};
+
 // Default form data state used in FileForm component.
 export const defaultFileFormValues: FileFormValues = {
 	series: '',
 	file: null,
 };
 
-// Default form data state used in DataForm component.
-export const defaultDataFormValues: DataFormValues = {
+// Default form data state used in InformationForm component.
+export const defaultInformationFormValues: InformationFormValues = {
 	series: '',
 	year: '',
 	description: '',
-};
-
-// Default form data state used in UpdateDOcsForm component.
-export const defaultUpdateDocsFormValues: UpdateDocsFormValues = {
-	series: '',
-	year: '',
-};
-
-// Default form data state used in ContactForm component.
-export const defaultContactFormValues: ContactFormValues = {
-	email: '',
-	message: '',
 };
 
 // Default form data state used in SeriesDataForm component.
@@ -45,6 +39,13 @@ export const defaultSeriesDataFormValues: SeriesDataFormValues = {
 	documents_url: '',
 };
 
+// Default form data state used in SignInForm component.
 export const defaultSignInFormValues: LoginFormValues = {
 	password: '',
+};
+
+// Default form data state used in UpdatePenaltiesForm component.
+export const defaultUpdatePenaltiesFormValues: UpdatePenaltiesFormValues = {
+	series: '',
+	year: '',
 };
